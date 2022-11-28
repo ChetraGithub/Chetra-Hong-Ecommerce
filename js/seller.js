b3f67cf66143760b24faf1bae185278521d3e2d5
ChetraGithub <hongchetra22@gmail.com>
2022-11-28 21:04:07 +0700

    #3 Display products

--- js/seller.js
+++ js/seller.js
@@ -1,14 +1,20 @@
 // DOM ELEMENTS ===========================================================
 const dom_dialog = document.querySelector("#dialog");
+let dom_display_product = document.querySelector("#display-product");
+
+// VARIABLES ==============================================================
 let getName = document.querySelector("#pdt-name");
 let getType = document.querySelector("#pdt-type");
 let getPrice = document.querySelector("#pdt-price");
 let getDiscription = document.querySelector("#pdt-discription");
 let getComment = document.querySelector("#pdt-comment");
 let getImage = document.querySelector("#pdt-image");
+let numberOfProduct = 0;
 
 // DATABASE ===============================================================
-let products = [];
+let products = [{name: "Mac Book Pro 2022", type: "MSI",price: 1999.99, discription: "12G Ram, 1TB M1",comment: "From China",image: "C/"}, 
+                {name: "MSI T 450", type: "MSI",price: 750, discription: "16G Ram, 2TB SSD",comment: "From China",image: "C/"},
+                {name: "TUF Gaming Pro 2022", type: "MSI",price: 1020, discription: "12G SSD, 520G SSD",comment: "From China",image: "C/"}];
 
 // FUNCTIONS ==============================================================
 // Show dialog ------------------------------------------------------------
@@ -21,14 +27,14 @@ function onHide(event) {
     dom_dialog.style.display = "none";
 }
 
-// Create product ------------------------------------------------------------
+// Create products --------------------------------------------------------
 function createProduct() {
     let checkDataField = getName.value && getType.value && getPrice.value && getDiscription.value && getComment.value && getImage.value;
     if (checkDataField) {
         let product = {};
         product.name = getName.value;
         product.type = getType.value;
-        product.price = getPrice.value;
+        product.price = parseInt(getPrice.value);
         product.discription = getDiscription.value;
         product.comment = getComment.value;
         product.image = getImage.value;
@@ -38,6 +44,59 @@ function createProduct() {
     else {
         window.alert("Please fill all the fields!");
     }
+}
+renderProducts();
+
+// Render products --------------------------------------------------------
+function renderProducts() {
+    numberOfProduct += 1;
+
+    for (let i = 0; i < products.length; i++) {
+        let item = document.createElement("div");
+        item.className = "item";
+    
+        let number = document.createElement("div");
+        number.className = "number";
+    
+        let uid = document.createElement("span");
+        uid.id = "uid";
+        uid.textContent = numberOfProduct;
+        number.appendChild(uid);
+    
+        let name = document.createElement("span");
+        name.id = "name";
+        name.textContent = products[i].name;
+    
+        let price = document.createElement("span");
+        price.id = "price";
+        price.textContent = products[i].price + " $";
+        
+        let discription = document.createElement("span");
+        discription.id = "discription";
+        discription.textContent = products[i].discription;
+
+        let modify = document.createElement("div");
+        modify.id = "modify";
+
+        let btn_edit = document.createElement("img");
+        btn_edit.src = "../images/edit.png";
+        modify.appendChild(btn_edit);
+
+        let btn_delete = document.createElement("img");
+        btn_delete.src = "../images/delete.png";
+        modify.appendChild(btn_delete);
+
+        // Add to item
+        item.appendChild(number);
+        item.appendChild(name);
+        item.appendChild(price);
+        item.appendChild(discription);
+        item.appendChild(modify);
+
+        // Add to Dom Display
+        dom_display_product.appendChild(item)
+        console.log(item)
+    };
 }
 
 // ADD EVENTS =============================================================
